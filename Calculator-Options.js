797623e763c182b8d9de9ef8fe91df78bf0e7a0d
cmdbris <ebrischri@gmail.com>
2023-12-30 01:09:14 -0500

    Began adding dynamic colour inputs

--- Calculator-Options.js
+++ Calculator-Options.js
@@ -1,27 +1,32 @@
+// Disabler and Enabler of Classes
+
+
+
 // Animation checkbox functions
 
 let checkbox_ToggleAnimation = document.getElementById("animation-toggle");
-let calc_div = document.getElementsByClassName("calculator-table")[0];
+let calculatorTable = document.getElementsByClassName("calculator-table")[0];
 
 checkbox_ToggleAnimation.addEventListener("change", function () {
-    calc_div.classList.toggle('enable-hover', checkbox_ToggleAnimation.checked);
+    calculatorTable.classList.toggle('enable-hover', checkbox_ToggleAnimation.checked);
 });
 
 // Colour changing checkboxes functions
 
 let checkbox_TogglePresetColour = document.getElementById('preset-colour-toggle');
 let checkbox_ToggleCustomColour = document.getElementById('custom-colour-toggle');
+let presetColourOptions = document.getElementById('preset-colour-options-placeholder');
+let customColourOptions = document.getElementById('custom-colour-options-placeholder');
 
-checkbox_TogglePresetColour.addEventListener("change", function (event) {
-    let targetCheckbox = event.target;
-    let checkbox = targetCheckbox.getAttribute('checkbox-data');
-    updateCheckboxes(checkbox);
+presetColourOptions.classList.remove('preset-colour-options');
+customColourOptions.classList.remove('custom-colour-options-placeholder');
+
+checkbox_TogglePresetColour.addEventListener("change", function () {
+    updateCheckboxes('preset');
 });
 
-checkbox_ToggleCustomColour.addEventListener("change", function (event) {
-    let targetCheckbox = event.target;
-    let checkbox = targetCheckbox.getAttribute('checkbox-data');
-    updateCheckboxes(checkbox);
+checkbox_ToggleCustomColour.addEventListener("change", function () {
+    updateCheckboxes('custom');
 });
 
 function updateCheckboxes(checkbox) {
@@ -29,7 +34,12 @@ function updateCheckboxes(checkbox) {
     // If one checkbox is checked, the other is unchecked
     if (checkbox === 'preset' && checkbox_TogglePresetColour.checked) {
         checkbox_ToggleCustomColour.checked = false;
-      } else if (checkbox === 'custom' && checkbox_ToggleCustomColour.checked) {
+        customColourOptions.classList.remove();
+        presetColourOptions.classList.add();
+    }
+    else if (checkbox === 'custom' && checkbox_ToggleCustomColour.checked) {
         checkbox_TogglePresetColour.checked = false;
-      }
+        presetColourOptions.classList.remove();
+        customColourOptions.classList.add();
+    }
 }
